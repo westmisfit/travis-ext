995440b807969a7b3ccb165fec948574771db06c
West Wang <west@misfit.com>
2015-07-03 17:02:43 +0800

    fix install awscli in mac os

--- share/install_awscli.sh
+++ share/install_awscli.sh
@@ -1,5 +1,8 @@
 source $TRAVIS_EXT_HOME/$PROJECT_LANGUAGE/vars
 
-sudo apt-get update
-sudo apt-get install -y python-pip 
+# sudo apt-get update
+# sudo apt-get install -y python-pip 
+# sudo pip install awscli
+
+sudo easy_install pip
 sudo pip install awscli
