40d2dde901f464c5aff7892e1db44e77eeff9080
West Wang <west@misfit.com>
2015-07-03 16:04:37 +0800

    alert for not supported command

--- travis-ext.sh
+++ travis-ext.sh
@@ -9,6 +9,24 @@ TRAVIS_EXT_HOME="${TRAVIS_EXT_HOME-${HOME}/travis-ext}"
 
 COMMAND="$1"
 
+# print help message
+function help
+{
+    if [[ -f "$TRAVIS_EXT_HOME/$PROJECT_LANGUAGE/help.sh" ]]; then
+        # run script by different language
+        bash -e$TRAVIS_EXT_OPTIONS $TRAVIS_EXT_HOME/$PROJECT_LANGUAGE/help.sh
+    fi
+}
+
+# verify environment variables
+function verify_vars
+{
+    if [[ -f "$TRAVIS_EXT_HOME/$PROJECT_LANGUAGE/verify_vars.sh" ]]; then
+        # run script by different language
+        bash -e$TRAVIS_EXT_OPTIONS $TRAVIS_EXT_HOME/$PROJECT_LANGUAGE/verify_vars.sh
+    fi
+}
+
 if [[ "TRAVIS_EXT_HOME" = "" ]]; then
     echo "Not setted 'TRAVIS_EXT_HOME'"
     exit 1
@@ -17,14 +35,14 @@ fi
 if [[ "PROJECT_LANGUAGE" = "" ]]; then
     echo "Not setted 'PROJECT_LANGUAGE'"
     exit 1
-elif [[ -f "$TRAVIS_EXT_HOME/$PROJECT_LANGUAGE/verify_vars.sh" ]]; then
-    # verify environment by different language
-    bash -e$TRAVIS_EXT_OPTIONS $TRAVIS_EXT_HOME/$PROJECT_LANGUAGE/verify_vars.sh
+else
+    # verify environment variables by different language
+    verify_vars
 fi
 
 if [[ "COMMAND" = "" ]]; then
     # print help message if no command
-    COMMAND="help"
+    help
 fi
 
 if [[ -f "$TRAVIS_EXT_HOME/$PROJECT_LANGUAGE/$COMMAND.sh" ]]; then
@@ -33,4 +51,8 @@ if [[ -f "$TRAVIS_EXT_HOME/$PROJECT_LANGUAGE/$COMMAND.sh" ]]; then
 elif [[ -f "$TRAVIS_EXT_HOME/share/$COMMAND.sh" ]]; then
     # run script shared with all languages
     bash -e$TRAVIS_EXT_OPTIONS $TRAVIS_EXT_HOME/share/$COMMAND.sh
+else
+    echo "Not supported command: $COMMAND"
+    help
+    exit 1
 fi
