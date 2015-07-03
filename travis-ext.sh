e414b0d88ab28b3111ae70c5fb96838a91df2a34
West Wang <west@misfit.com>
2015-07-03 12:30:18 +0800

    modify travis-ext.sh

--- travis-ext.sh
+++ travis-ext.sh
@@ -1,11 +1,32 @@
 #!/bin/bash -e
 
 TRAVIS_EXT_HOME=${TRAVIS_EXT_HOME-${HOME}/travis-ext}
-BASH="bash -ex"
+# TRAVIS_EXT_OPTIONS="x"  # set 'x' to print all shell scripts
+
 COMMAND="$1"
 
+if [[ "TRAVIS_EXT_HOME" = "" ]]; then
+    echo "Not setted 'TRAVIS_EXT_HOME'"
+    exit 1
+fi
+
+if [[ "PROJECT_LANGUAGE" = "" ]]; then
+    echo "Not setted 'PROJECT_LANGUAGE'"
+    exit 1
+elif [[ -f "$TRAVIS_EXT_HOME/$PROJECT_LANGUAGE/verify_vars.sh" ]]; then
+    # verify environment by different language
+    bash -e$TRAVIS_EXT_OPTIONS $TRAVIS_EXT_HOME/$PROJECT_LANGUAGE/verify_vars.sh
+fi
+
+if [[ "COMMAND" = "" ]]; then
+    # print help message if no command
+    COMMAND="help"
+fi
+
 if [[ -f "$TRAVIS_EXT_HOME/$PROJECT_LANGUAGE/$COMMAND.sh" ]]; then
-    $BASH $TRAVIS_EXT_HOME/$PROJECT_LANGUAGE/$COMMAND.sh
+    # run script by different language
+    bash -e$TRAVIS_EXT_OPTIONS $TRAVIS_EXT_HOME/$PROJECT_LANGUAGE/$COMMAND.sh
 elif [[ -f "$TRAVIS_EXT_HOME/share/$COMMAND.sh" ]]; then
-    $BASH $TRAVIS_EXT_HOME/share/$COMMAND.sh
+    # run script shared with all languages
+    bash -e$TRAVIS_EXT_OPTIONS $TRAVIS_EXT_HOME/share/$COMMAND.sh
 fi
